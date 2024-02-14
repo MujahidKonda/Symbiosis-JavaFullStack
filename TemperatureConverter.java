8c64cff017a5ca13016468d69a2ddf2e80d6567f
MujahidKonda <mujahid9175401591@gmail.com>
2024-02-14 12:12:16 +0530

    P1

---
+++ TemperatureConverter.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class temperature{
+	public static void main(String cp[]){
+		System.out.print( "temperature in Fahrenheit :");
+		Scanner sc = new Scanner(System.in);
+		float F = sc.nextInt();
+		float C = ( F - 32 ) * 5/9 ;
+		System.out.println("Temperature in Celcius is :" +C );
+	}
+}
