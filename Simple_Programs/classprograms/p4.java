520420407fff9e69a76bc878eeeb9253ad86e135
MujahidKonda <mujahid9175401591@gmail.com>
2024-02-23 16:47:59 +0530

    23-02-2024

--- Simple_Programs/classprograms/p4.java
+++ Simple_Programs/classprograms/p4.java
@@ -4,15 +4,20 @@ import java.util.Scanner;
 
 class evenodd{
     public void eo(){
-        System.out.print("Enter the Nmber :");
+        System.out.print("Enter the Number :");
         Scanner sc = new Scanner(System.in);
         int input = sc.nextInt();
 
-        if( input%2 == 0 ){
-            System.out.print("The Number is Even");
+        if( input != 0){
+            if( input%2 == 0 ){
+                System.out.print("The Number is Even");
+            }
+            else{
+                System.out.print("The Number is Odd");
+            }
         }
         else{
-            System.out.print("The Number is Odd");
+            System.out.print("The Number is Neutral");
         }
         sc.close();
     }
