bbe859b200e3630666aab0d112568b5b9fa4ccc1
MujahidKonda <mujahid9175401591@gmail.com>
2024-02-27 16:26:15 +0530

    27-02-2024

--- Simple_Programs/classprograms/p17.java
+++ Simple_Programs/classprograms/p17.java
@@ -1,5 +1,21 @@
 package classprograms;
 
-class p17 {
-    
+class test {
+    private int id;
+
+    void setid(int i){
+        id = i;
+    }
+
+    int getid(){
+        return id;
+    }
+}
+
+class p17{
+    public static void main(String args[]){
+        test t = new test();
+        t.setid(5);
+        System.out.print("Id is = "+t.getid());
+    }
 }
