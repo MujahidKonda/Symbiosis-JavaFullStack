3f29d628bc70ebd87fe6ebabc6ee913deb2b1c94
MujahidKonda <mujahid9175401591@gmail.com>
2024-03-05 16:14:48 +0530

    05-03-2024

---
+++ Simple_Programs/classprograms/p23.java
@@ -0,0 +1,19 @@
+package classprograms;
+
+class p23 {
+    public static float pi(){
+        return 3.142f;
+    }
+    public static int sum (int a, int b) {
+        int c;
+        c = a +b;
+        return c;
+    }
+
+    public static void main(String[] args){
+        int x = 10, y = 20, z;
+        System.out.println("The Value of PI is 22/7 = "+pi());
+        z = sum(x,y);
+        System.out.println("Sum of " +x+ " and "+y+ " is "+z);
+    }
+}
