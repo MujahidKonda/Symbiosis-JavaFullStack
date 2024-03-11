a2f64f1564eb1d104d6d079de31f42cab507d67b
MujahidKonda <mujahid9175401591@gmail.com>
2024-03-11 16:23:15 +0530

    11-03-2024

---
+++ Simple_Programs/classprograms/p24.java
@@ -0,0 +1,9 @@
+package classprograms;
+
+public class p24 {
+    int x = 5;
+    public static void main(String[] args){
+        p24 myObj = new p24();
+        System.out.println(myObj.x);
+    }
+}
