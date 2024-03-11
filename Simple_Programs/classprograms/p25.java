ded40da28a53d0fc95977fb54e18f6d44db2f392
MujahidKonda <mujahid9175401591@gmail.com>
2024-03-11 16:53:21 +0530

    11-03-2024

---
+++ Simple_Programs/classprograms/p25.java
@@ -0,0 +1,12 @@
+package classprograms;
+
+class first {
+    int x = 5; 
+}
+
+class simple{
+    public static void main(String[] args){
+        first myObj = new first();
+        System.out.print(myObj.x);
+    }
+}
