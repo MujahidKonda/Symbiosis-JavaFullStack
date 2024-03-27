dce08d50999dcc5d35a036ac035f9d5a85fa7e5c
MujahidKonda <mujahid9175401591@gmail.com>
2024-03-27 12:27:33 +0530

    27-03-2024

--- Simple_Programs/classprograms/p47.java
+++ Simple_Programs/classprograms/p47.java
@@ -1,5 +1,28 @@
 package classprograms;
 
+class thready implements Runnable{
+    public void run(){
+        System.out.println("Thread is "+Thread.currentThread().getName());
+        Thread.yield();
+        try{
+            for(int i = 0; i<=10; i++){
+                System.out.println("thread 1 value is : "+i);
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
+
 class p47 {
-    
+    public static void main(String[] args){
+        thready t1 = new thready();
+        Thread th1 = new Thread(t1);
+        th1.start();
+        System.out.println("Thread is "+Thread.currentThread().getName());
+        for(int i = 0; i<=10; i++){
+            System.out.println("Main Thread value is : "+i);
+        }
+    }
 }
